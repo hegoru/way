71f065c821b7edf0c1f2c3872a5ef91383543d4d
hegoru <59119282+hegoru@users.noreply.github.com>
2020-06-25 20:46:48 +0300

    Add files via upload

---
+++ basis.rb
@@ -0,0 +1,3 @@
+# Ruby doesn't care about the comments. All comments starts with `#` in Ruby
+print "Easy output with print\n" # Method to output given string
+puts "Easy output with puts" # Aternative method to output given string
