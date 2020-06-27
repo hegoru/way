93e329074fde384f4a05081019813863134040ca
hegoru <59119282+hegoru@users.noreply.github.com>
2020-06-27 20:02:01 +0300

    Update comments.rb

--- comments.rb
+++ comments.rb
@@ -1,11 +1,12 @@
 # Comment is any text that can't be executed and ignored by interpreter in Ruby.
 # In a few words: comments are the notes of a programmer.
 #
+# The main role of comments is to explain a part of the code or to make a quick note. 
+#
 # There are two types of comments: single line and multi line.
-# Single line comment is a simple line of text after hash (#) character.
+# Single line comment is a line of text after hash (#) character.
 
 =begin
 There is a multiline comment.
 It contains the lines between 'begin' and 'end' comment-markers.
-Use it a little.
 =end
